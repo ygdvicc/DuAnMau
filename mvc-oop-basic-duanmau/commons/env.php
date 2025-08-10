272ce1340611c0234e2eb27618642a5b193d3d7c
ygdvicc <dohoangviet06122004@gmail.com>
2025-08-10 23:48:48 +0700

    lan 1

--- mvc-oop-basic-duanmau/commons/env.php
+++ mvc-oop-basic-duanmau/commons/env.php
@@ -3,12 +3,12 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/duanmau/mvc-oop-basic/');
+define('BASE_URL'       , 'http://localhost/phpmyadmin/index.php?route=/database/structure&db=duanmau');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'database');  // Tên database
+define('DB_NAME'    , 'duanmau');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
